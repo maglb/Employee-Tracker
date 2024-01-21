093dc7a9bbb8c368e0e84bdba8099e60e64c91e9
Maglb <magalilebon@Magalis-MBP.lan>
2024-01-20 19:36:54 -0700

    queries.sql file updated with working code to merge tables

--- db/queries.sql
+++ db/queries.sql
@@ -1,3 +1,26 @@
-SELECT department_name
-FROM departments
-JOIN roles ON departments_id = department_id.roles;
+-- SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, employees.manager_id 
+-- FROM employees
+-- JOIN roles ON employees.role_id = roles.id;
+
+-- -- Combine name for managers
+-- SELECT a.id, a.first_name, a.last_name,
+-- CONCAT(b.first_name," ", b.last_name) AS manager
+-- FROM employees AS a
+-- LEFT JOIN employees AS b
+-- ON a.manager_id = b.id;
+
+
+
+SELECT a.id, a.first_name, a.last_name, roles.title, roles.salary,
+-- Combine first and last name of the employees and set them under manager
+CONCAT(b.first_name," ", b.last_name) AS manager 
+-- Diplay the full name of the manager in lieu of manager_id
+FROM employees AS a
+LEFT JOIN employees AS b ON a.manager_id = b.id
+JOIN roles ON a.role_id = roles.id;
+
+
+
+
+
+
