df79a0fdcad99fe51ac0b2c0af6defd101f53ac3
maglb <magali.lb21@gmail.com>
2024-02-24 12:33:08 -0700

    View all roles function added and working

--- roles.js
+++ roles.js
@@ -2,11 +2,10 @@ const express = require("express");
 
 // See all roles
 const viewRoles = (connection) => {
-  const allRoles = `SELECT a.id, a.first_name, a.last_name, roles.title, roles.salary,
-CONCAT(b.first_name," ", b.last_name) AS manager 
-FROM employees AS a
-LEFT JOIN employees AS b ON a.manager_id = b.id
-JOIN roles ON a.role_id = roles.id;`;
+  const allRoles = `SELECT roles.id, roles.title, departments.department_name, roles.salary
+FROM roles
+JOIN departments ON departments.id = roles.department_id
+ORDER BY roles.id ASC;`;
 
   connection
     .query(allRoles)
