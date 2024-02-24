2c893577177246c8b9701f4bdc8376b54af013ab
maglb <magali.lb21@gmail.com>
2024-02-24 12:32:59 -0700

    View all department function added and working

--- departments.js
+++ departments.js
@@ -2,11 +2,7 @@ const express = require("express");
 
 // See all departments
 const viewDepartments = (connection) => {
-  const allDepartments = `SELECT a.id, a.first_name, a.last_name, roles.title, roles.salary,
-CONCAT(b.first_name," ", b.last_name) AS manager 
-FROM employees AS a
-LEFT JOIN employees AS b ON a.manager_id = b.id
-JOIN roles ON a.role_id = roles.id;`;
+  const allDepartments = `SELECT * FROM departments;`;
 
   connection
     .query(allDepartments)
