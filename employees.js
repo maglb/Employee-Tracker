07cb9ba80c51015d767267d4686d1b4bf6272036
maglb <magali.lb21@gmail.com>
2024-03-01 12:48:50 -0700

    Choices added for role and manager input

--- employees.js
+++ employees.js
@@ -19,45 +19,47 @@ JOIN roles ON a.role_id = roles.id;`;
     });
 };
 
-const addEmployees = () => {
-  newEmployee().then((data) => {
-    const addInfo = `INSERT INTO employees (first_name, last_name, role_id, manager_id)
-  VALUES (${first_name}, ${last_name}, ${role_id}, ${manager_id});`;
+const addEmployees = (connection, data) => {
+  const addInfo = `INSERT INTO employees (first_name, last_name, role_id, manager_id)
+  VALUES ('${data.first_name}', '${data.last_name}', ${data.role_id}, ${data.manager_id});`;
 
-    connection
-      .query(allEmployees)
-      .then(function (results) {
-        console.table(results[0]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+  connection
+    .query(addInfo)
+    .then(function (results) {
+      console.table(results[0]);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 const newEmployee = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "first_name",
-      message: "What is the new employee's first name?",
-    },
-    {
-      type: "input",
-      name: "last_name",
-      message: "What is the new employee's last name?",
-    },
-    {
-      type: "input",
-      name: "role",
-      message: "What is the new employee's role?",
-    },
-    {
-      type: "input",
-      name: "manager",
-      message: "What is the new employee's manager?",
-    },
-  ]);
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "What is the new employee's first name?",
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "What is the new employee's last name?",
+      },
+      {
+        type: "options",
+        name: "role",
+        choices: ["Software Engineer", "Account Manager", "Accountant", "Legal Team Lead", "Lawyer", "Lead Software Engineer", "Creative Director", "Copy Writer", "Sales Lead", "Salesperson"],
+      },
+      {
+        type: "options",
+        name: "manager",
+        choices: ["None", "Briana Lowel", "John Meyer", "Jessica Spears", "Katherine Grey", "Robert Smith", "Peter McDonals", "Bryan Ruth", "Hannah Gold"],
+      },
+    ])
+    .then((data) => {
+      addEmployees(data);
+    });
 };
 
 module.exports = { viewEmployees, addEmployees };
