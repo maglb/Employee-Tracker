ac49233189079630a74396cb1dfd13df0fda67c1
maglb <magali.lb21@gmail.com>
2024-03-02 11:14:38 -0700

    function to add new role completed

--- roles.js
+++ roles.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const inquirer = require("inquirer");
 
 // See all roles
 const viewRoles = (connection) => {
@@ -17,4 +18,51 @@ ORDER BY roles.id ASC;`;
     });
 };
 
-module.exports = { viewRoles };
+// Get data about new role
+const addRole = (connection) => {
+  // Get data for all roles
+  // connection.query('SELECT id, title FROM roles');
+  return Promise.all([
+    connection.query("SELECT id AS value, title AS name FROM roles"),
+  ])
+    .then((result) => {
+      const departments = result[0][0];
+      console.log(departments);
+      return inquirer.prompt([
+        {
+          type: "input",
+          name: "role",
+          message: "What is the name of the role?",
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "What is the salary of the role?",
+        },
+        {
+          type: "list",
+          name: "department",
+          message: "Which department does the role belong to?",
+          choices: departments,
+        },
+      ]);
+    })
+    .then((data) => {
+      console.log(data);
+
+      const addInfo = `INSERT INTO roles (title, department_id, salary) VALUES (?, ?, ?);`;
+      return connection.query(addInfo, [
+        data.role,
+        data.department,
+        data.salary,
+      ]);
+    })
+    .then(function (results) {
+      console.table(results[0]);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = { viewRoles, addRole };
